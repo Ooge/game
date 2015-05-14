5df1b0ab8ad91be4d8100989e424bb0e9ece4561
Ryan Thorn <ryan@ryanthorn.uk>
2015-05-14 22:45:27 +0100

    Circles everywhere

--- assets/js/global.js
+++ assets/js/global.js
@@ -202,8 +202,10 @@ var Player = function(x, y, boundX, boundY, speed) {
 
 Player.prototype.render = function() {
 	var app = Ooge.global;
-	app.ctx.fillStyle = 'rgb(255,0,0)';
-	app.ctx.fillRect(this.x, this.y, this.boundX, this.boundY);
+	app.ctx.fillStyle = 'rgb('+Math.floor(Math.random() * 255) + 1+','+Math.floor(Math.random() * 255) + 1+','+Math.floor(Math.random() * 255) + 1+')';
+	ctx.beginPath();
+    ctx.arc(this.x,this.y,this.boundX,0,Math.PI*2,true);
+    ctx.fill();
 };
 
 Player.prototype.update = function() {
